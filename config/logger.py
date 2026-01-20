0f106c051e3c3622a0f16aa3287c9b8c04e45157
rabia zia nezami <rabianezami786@gail.com>
2026-01-20 23:10:18 +0430

    feat: add logger.py

---
+++ config/logger.py
@@ -0,0 +1,26 @@
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+
+LOG_FILE = "output/crewai.log"
+os.makedirs(os.path.dirname(LOG_FILE), exist_ok=True)
+
+logger = logging.getLogger("CrewAI")
+logger.setLevel(logging.DEBUG)
+
+file_handler = RotatingFileHandler(
+    LOG_FILE, maxBytes=1_000_000, backupCount=3, encoding="utf-8"
+)
+file_handler.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    "%(asctime)s | %(levelnae)s | %(name)s | %(message)s"
+)
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
